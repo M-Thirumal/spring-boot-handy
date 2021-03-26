a5597a0089e3be1b81e80c3db14937e36f9dd878
Thirumal <m.thirumal@hotmail.com>
2021-03-26 16:07:44 +0530

    Generate xml

--- src/main/java/in/thirumal/xmlignore/EmployeeController.java
+++ src/main/java/in/thirumal/xmlignore/EmployeeController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
 /**
  * @author Thirumal
  *
@@ -22,4 +25,13 @@ public class EmployeeController {
 		return Employee.builder().id(1L).name("Thirumal").dob(LocalDate.now()).build();
 	}
 	
+	@GetMapping("/generate")
+	public String generateXML() throws JsonProcessingException {
+		Employee employee = Employee.builder().id(1L).name("Thirumal").dob(LocalDate.now()).build();
+		XmlMapper xmlMapper = new XmlMapper();
+		//xmlMapper.setAnnotationIntrospector(new XmlAnnotationIntrospector());
+		String xml = xmlMapper.writer().withRootName("Emp").withDefaultPrettyPrinter().writeValueAsString(employee);
+		System.out.println(xml);
+		return xml;
+	}
 }
